66d3e48b0a7927bed11695d63c4ac8e900dd766b
resb53 <resb53.git@gmail.com>
2022-10-10 16:38:29 +0100

    Threaded receiver

--- udp_receiver.py
+++ udp_receiver.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!usr/bin/env python3
 
 import pickle
-import signal
 import socket
 import sys
+import threading
 
+stopping = True
 server = "127.0.0.1"
 port = 20777
 packets = []
@@ -14,24 +15,37 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((server, port))
 
-    try:
-        receive(sock)
-    except Exception:
-        shutdown(None, None)
+    x = threading.Thread(target=receive, args=(sock,))
+    x.start()
 
-
-def shutdown(signal, frame):
-    print("Writing data...")
-    print("Shutting down...")
+    finish()
 
 
 def receive(sock):
     # with open('telemetry.bin', 'wb') as f:
+    print("Listening...")
     while True:
         data, address = sock.recvfrom(2048)
         packets.append(data)
+        global stopping
+        if stopping:
+            break
+    with open("telemetry.pickle", 'wb') as f:
+        pickle.dump(packets, f)
+    print("Telemetry saved.")
+
+
+def finish():
+    while True:
+        print("Type 'exit' to finish")
+        ui = str(input())
+        if ui == "exit":
+            global stopping
+            stopping = True
+            terminate = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            terminate.sendto(bytes("FINISH", "utf-8"), (server, port))
+            break
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGBREAK, shutdown)
     main()
