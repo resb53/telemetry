eacbd52067ab6494838e49128fbd4bbcd8323882
resb53 <resb53.git@gmail.com>
2022-10-10 11:25:25 +0100

    Attempt to close receiver gracefully

--- parse_udp.py
+++ parse_udp.py
@@ -19,11 +19,49 @@ packetSize = {
     11: 1155
 }
 
+eventSize = {
+    "SSTA": 0,
+    "SEND": 0,
+    "FTLP": 5,
+    "RTMT": 1,
+    "DRSE": 0,
+    "DRSD": 0,
+    "TMPT": 1,
+    "CHQF": 0,
+    "RCWN": 1,
+    "PENA": 7,
+    "SPTP": 12,
+    "STLG": 1,
+    "LGOT": 0,
+    "DTSV": 1,
+    "SGSV": 1,
+    "FLBK": 8,
+    "BUTN": 4
+}
+
+line = 1
+
 with open("./telemetry.bin", "rb") as telem:
-    # while (telem.peek(1) != b""):
-    header = telem.read(24)
+    while (telem.peek(1) != b""):
+        header = telem.read(24)
 
-    # [uint16,uint8,uint8,uint8,uint8,uint64,float32,uint32,uint8,uint8]
-    sep = struct.unpack("HBBBBfIBB", header)
+        # [uint16,uint8,uint8,uint8,uint8,uint64,float32,uint32,uint8,uint8]
+        hfields = struct.unpack("=H4BQfI2B", header)
+        print(f"{line}: {hfields}")
+        print(hfields[4])
 
-    print(sep)
+        body = telem.read(packetSize[hfields[4]])
+
+        if hfields[4] == 3:
+            event = struct.unpack("=4s", body)
+            event = event[0].decode("utf8")
+            print(event)
+            body = telem.read(eventSize[event])
+
+        footer = telem.read(8)
+        print(footer)
+
+        if line == 5:
+            break
+        else:
+            line += 1
--- udp_receiver.py
+++ udp_receiver.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
 
+import pickle
+import signal
 import socket
+import sys
 
 server = "127.0.0.1"
 port = 20777
+packets = []
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((server, port))
 
-with open('telemetry.bin', 'wb') as f:
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((server, port))
+
+    try:
+        receive(sock)
+    except Exception:
+        shutdown(None, None)
+
+
+def shutdown(signal, frame):
+    print("Writing data...")
+    print("Shutting down...")
+
+
+def receive(sock):
+    # with open('telemetry.bin', 'wb') as f:
     while True:
-        data, address = sock.recvfrom(4096)
-        f.write(data)
+        data, address = sock.recvfrom(2048)
+        packets.append(data)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGBREAK, shutdown)
+    main()
