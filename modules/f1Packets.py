e5748356e6237ed2b0e1043292bb50d9d78745e4
resb53 <resb53.git@gmail.com>
2022-10-17 19:01:16 +0100

    Tidier bodywork

--- modules/f1Packets.py
+++ modules/f1Packets.py
@@ -21,6 +21,27 @@ class Header():
         self.p2car = header[9]
 
 
+class Body():
+    def __init__(self, body):
+        self.values = body
+        self.index = 0
+        self.length = len(body)
+
+    def __len__(self):
+        return self.length
+
+    def read(self, n):
+        if n > (self.length - self.index):
+            raise IndexError(f"{n} bytes requested where only {self.length - self.index} remain")
+        else:
+            r = self.values[self.index:self.index+n]
+            self.index += n
+            return r
+
+    def reset(self):
+        self.index = 0
+
+
 class Packet():
     def __init__(self, header, body):
         # Import Header values into packet
@@ -29,10 +50,10 @@ class Packet():
         # Check Size
         if (len(body) + 24) != packetSize[self.type]:
             raise ValueError(f"Packet type {header.type} has size {len(body) + 24}. Should be {packetSize[self.type]}")
-        self.body = body
+        self.body = Body(body)
 
     def __str__(self):
-        return(f"{self.timestamp}: {self.body}")
+        return(f"{self.timestamp}: {self.body.values}")
 
 
 class MotionPacket(Packet):
@@ -40,12 +61,11 @@ class MotionPacket(Packet):
         super().__init__(header, body)
         self.cars = []
         for _ in range(22):
-            car = list(struct.unpack("=6f6h6f", body[0:60]))
+            car = list(struct.unpack("=6f6h6f", self.body.read(60)))
             for i, x in enumerate(car[6:12]):
                 car[i+6] = x / 32767.0
             self.cars.append(tuple(car))
-            body = body[60:]
-        self.motion = struct.unpack("=15f", body[0:60])
+        self.motion = struct.unpack("=15f", self.body.read(60))
 
 
 class SessionPacket(Packet):
@@ -56,28 +76,22 @@ class SessionPacket(Packet):
             self.trackLength, self.seshType, self.trackId, self.formula,
             self.seshTimeLeft, self.seshDuration, self.pitSpeed, self.paused,
             self.spectating, self.spectIndex, self.sli, self.marshalZoneCount
-        ) = struct.unpack("=B2bBHBbB2H6B", body[0:19])
-        body = body[19:]
+        ) = struct.unpack("=B2bBHBbB2H6B", self.body.read(19))
         zones = []
         for _ in range(21):
-            zones.append(struct.unpack("=fb", body[0:5]))
-            body = body[5:]
-        (self.safetyCar, self.network, self.forecastSampleCount) = struct.unpack("=3B", body[0:3])
-        body = body[3:]
+            zones.append(struct.unpack("=fb", self.body.read(5)))
+        (self.safetyCar, self.network, self.forecastSampleCount) = struct.unpack("=3B", self.body.read(3))
         forecast = []
         for _ in range(56):
-            forecast.append(struct.unpack("=3B4bB", body[0:8]))
-            body = body[8:]
+            forecast.append(struct.unpack("=3B4bB", self.body.read(8)))
         (
             self.forecastAccuracy, self.aiDifficulty, self.seasonLinkId, self.weekendLinkId,
             self.sessionLinkId, self.pitIdeal, self.pitLatest, self.pitRejoin
-        ) = struct.unpack("=2B3L3B", body[0:17])
-        body = body[17:]
-        self.assists = struct.unpack("=9B", body[0:9])
-        body = body[9:]
+        ) = struct.unpack("=2B3L3B", self.body.read(17))
+        self.assists = struct.unpack("=9B", self.body.read(9))
         (
             self.mode, self.rules, localTime, self.seshLength
-        ) = struct.unpack("=2BLB", body[0:7])
+        ) = struct.unpack("=2BLB", self.body.read(7))
         self.localTime = f"{localTime // 60 :02d}:{localTime % 60 :02d}"
 
 
@@ -89,8 +103,7 @@ class LapDataPacket(Packet):
 class EventPacket(Packet):
     def __init__(self, header, body):
         super().__init__(header, body)
-        self.event = struct.unpack("4s", body[0:4])[0].decode("utf-8")
-        self.body = body[4:]
+        self.event = struct.unpack("4s", self.body.read(4))[0].decode("utf-8")
 
     def __str__(self):
         return(f"{self.timestamp}: {self.event}, {self.body}")
@@ -99,14 +112,12 @@ class EventPacket(Packet):
 class ParticipantPacket(Packet):
     def __init__(self, header, body):
         super().__init__(header, body)
-        self.count = struct.unpack("B", body[0:1])[0]
-        body = body[1:]
+        self.count = struct.unpack("B", self.body.read(1))[0]
         self.cars = []
         for _ in range(22):
-            participant = list(struct.unpack("=7B48sB", body[0:56]))
+            participant = list(struct.unpack("=7B48sB", self.body.read(56)))
             participant[7] = participant[7].strip(b'\x00').decode("utf-8")
             self.cars.append(tuple(participant))
-            body = body[56:]
 
 
 class SetupPacket(Packet):
@@ -124,8 +135,7 @@ class StatusPacket(Packet):
         super().__init__(header, body)
         self.cars = []
         for _ in range(22):
-            self.cars.append(struct.unpack("=5B3f2H2BH3BbfB3fB", body[0:47]))
-            body = body[47:]
+            self.cars.append(struct.unpack("=5B3f2H2BH3BbfB3fB", self.body.read(47)))
 
 
 class ClassificationPacket(Packet):
